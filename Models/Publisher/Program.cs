d3605390060846e466381ee2f818a59e57e08268
tomeroth <tomeroth@op.pl>
2016-05-31 00:16:20 +0200

    first commit

---
+++ Models/Publisher/Program.cs
@@ -0,0 +1,30 @@
+﻿using EasyNetQ;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var bus = RabbitHutch.CreateBus("host=localhost"))
+            {
+                var input = "";
+                Console.WriteLine("Enter a message. 'Quit' to quit.");
+                while ((input = Console.ReadLine()) != "Quit")
+                {
+                    bus.Publish(
+                        new Message
+                    {
+                        Text = input
+                    });
+                }
+            }
+        }
+    }
+}
---
+++ Models/Subscriber/Program.cs
@@ -0,0 +1,31 @@
+﻿using EasyNetQ;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var bus = RabbitHutch.CreateBus("host=localhost"))
+            {
+                bus.Subscribe<Message>("test", HandleMessage);
+
+                Console.WriteLine("Listening for messages. Hit <return> to quit.");
+                Console.ReadLine();
+            }
+        }
+
+        static void HandleMessage(Message textMessage)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Got message: {0}", textMessage.Text);
+            Console.ResetColor();
+        }
+    }
+}
