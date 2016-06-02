dcafa4a4d9ae30af567a9e7841a357470788e9c2
tomeroth <tomeroth@op.pl>
2016-06-02 22:17:30 +0200

    publisher/subscriber

--- Models/Publisher/Program.cs
+++ Models/Publisher/Program.cs
@@ -15,14 +15,19 @@ namespace Publisher
             using (var bus = RabbitHutch.CreateBus("host=localhost"))
             {
                 var input = "";
+                Console.WriteLine("Enter your Name, 'Quit' to quit.");
+                var name = input = Console.ReadLine();
                 Console.WriteLine("Enter a message. 'Quit' to quit.");
                 while ((input = Console.ReadLine()) != "Quit")
                 {
+                    Console.WriteLine("Enter a message. 'Quit' to quit.");
                     bus.Publish(
                         new Message
                     {
-                        Text = input
+                           Name = name,
+                           Text = input
                     });
+
                 }
             }
         }
